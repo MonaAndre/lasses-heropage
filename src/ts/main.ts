8170ebafe6744c8fe3c7592f4b532e694a2cddcc
“Mona <monaelizabet@gmail.com>
2023-12-11 19:34:05 +0100

    :sparkles: quotes

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,2 +1,9 @@
 import '../style/style.scss';
+import { getQuote } from './services/axiosServices';
+
+const quoteBtn = document.getElementById("getbtn") as HTMLButtonElement;
+quoteBtn?.addEventListener("click",async ()=>{
+    await getQuote()
+});
+
 
---
+++ src/ts/services/axiosServices.ts
@@ -0,0 +1,18 @@
+import { IQuote } from "../Moduls/IQuote";
+import { get } from "./serviceBase";
+
+export const getQuote = async (): Promise<IQuote[]> => {
+  const url = "https://type.fit/api/quotes";
+  const data = await get<IQuote[]>(url);
+  const randomIndex = Math.floor(Math.random() * 16);
+  console.log(
+    "text:" + data[randomIndex].text,
+    "author:" + data[randomIndex].author
+  );
+  const title = document.createElement("h2") as HTMLHeadingElement;
+  const quoteContainer = document.getElementById("quote");
+  title.innerHTML = data[randomIndex].text;
+  quoteContainer?.appendChild(title);
+
+  return data;
+};
