2aa991608049b48f32247eb23c0a7b9214ea1493
“Mona <monaelizabet@gmail.com>
2023-12-12 16:59:15 +0100

    commit

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,2 +1,8 @@
 import '../style/style.scss';
+import { getQuote } from './services/axiosServices';
 
+const quoteBtn = document.getElementById("getbtn") as HTMLButtonElement;
+quoteBtn?.addEventListener("click",async ()=>{
+    await getQuote()
+    
+});
--- src/ts/services/serviceBase.ts
+++ src/ts/services/serviceBase.ts
@@ -3,4 +3,5 @@ import axios from "axios";
 export const get = async <T>(url: string): Promise<T> => {
   const response = await axios.get<T>(url);
   return response.data;
+  
 };
