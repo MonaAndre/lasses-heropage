b1e29a0aec0a8a3e98cf0d36f4d44bd2e227a198
Amanda Valencia <amandaavalencia@hotmail.com>
2023-12-13 11:42:49 +0100

    :sparkles: new weather feature

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,20 +1,38 @@
 import '../style/style.scss';
 import { weatherResponse } from './services/axiosServices';
 
-const weatherContainer = document.getElementById("weather");
-if(weatherContainer){
-   weatherContainer.appendChild
-    
-}
-
-
-const weatherReport = document.getElementById("weatherReport");
-
-
-
+//från HTML
+const weatherToday = document.getElementById("weatherToday");
+//<IWeatherRespons>
 const weatherForcast = await weatherResponse();
+//weatherContainer
+const weatherDiv = document.createElement("div");
+weatherDiv.className = "tempDiv";
+//Stockholm
+const cityName = document.getElementById("cityName") as HTMLParagraphElement;
+cityName.innerHTML = "Vädret i " + weatherForcast.name + " idag:";
 
+//IWeather
+const weatherIcon = document.createElement("img");
+const weatherDescription = document.createElement("p");
+const wind = document.createElement("p");
 
-const locationName = document.createElement("h2");
-locationName.innerHTML = weatherForcast.name;
-weatherReport?.appendChild(locationName);
+weatherIcon.src= "http://openweathermap.org/img/w/" + weatherForcast.weather[0].icon +".png";
+weatherDescription.innerHTML = weatherForcast.weather[0].description
+wind.innerHTML = "Vind: " +weatherForcast.wind.speed +" m/s";
+
+weatherDiv.appendChild(weatherIcon);
+weatherDiv.appendChild(weatherDescription);
+
+//main
+const pTagTemp = document.createElement("p");
+const pTagHumidity = document.createElement("p");
+
+pTagTemp.innerHTML= "Temperaturen idag: "+String(weatherForcast.main.temp)+"°" +" känns som: "+ String(weatherForcast.main.feels_like)+ "°";
+pTagHumidity.innerHTML= "Luftfuktighet: "+String(weatherForcast.main.humidity);
+
+weatherDiv.appendChild(pTagTemp);
+weatherDiv.appendChild(pTagHumidity);
+weatherDiv.appendChild(wind);
+weatherToday?.appendChild(weatherDiv);
+
--- src/ts/services/axiosServices.ts
+++ src/ts/services/axiosServices.ts
@@ -2,10 +2,8 @@ import { IWeatherResponse } from "../models/IWeatherResponse";
 import { get } from "./serviceBase";
 
 export const weatherResponse = async (): Promise<IWeatherResponse> =>{
-    const url = "https://api.openweathermap.org/data/2.5/weather?lat=41.881832&lon=-87.623177&appid=22c22c81677a5afd72f6f4410a4a4c57"
+    const url = "https://api.openweathermap.org/data/2.5/weather?lat=59.3293&lon=18.0686&appid=22c22c81677a5afd72f6f4410a4a4c57&units=metric"
     const data = await get<IWeatherResponse>(url);
-    console.log(data.main);
-    
     return data;
 }
 
