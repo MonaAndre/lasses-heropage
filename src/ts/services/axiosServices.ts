3eaffdc12162957568f92b906701a15a3faf3938
“Mona <monaelizabet@gmail.com>
2023-12-13 13:09:52 +0100

    fixed bugs

--- src/ts/services/axiosServices.ts
+++ src/ts/services/axiosServices.ts
@@ -21,6 +21,11 @@ export const drawQuote = (quote: IQuote) => {
   const author = document.querySelector(".quote__author") as HTMLHeadElement;
   author.innerHTML =  quote.author;
 };
+export const generateDogs = async () => {
+  const url = "https://api.thedogapi.com/v1/images/search?limit=3";
+  const data = await get<IDog[]>(url);
+  return data;
+};
 
 export const weatherResponse = async (): Promise<IWeatherResponse> =>{
     const url = "https://api.openweathermap.org/data/2.5/weather?lat=59.3293&lon=18.0686&appid=22c22c81677a5afd72f6f4410a4a4c57&units=metric"
