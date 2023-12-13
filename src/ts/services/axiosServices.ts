5e25f9549b49a22735322bfe9d29913e1c2b3c32
Amanda Valencia <amandaavalencia@hotmail.com>
2023-12-13 11:54:08 +0100

    :bug: fixed merge conflict

--- src/ts/services/axiosServices.ts
+++ src/ts/services/axiosServices.ts
@@ -1,11 +1,8 @@
 
 import { IQuote } from "../Moduls/IQuote";
-
 import { get } from "./serviceBase";
-
 import { IDog } from "../models/IDog";
-import { get } from "./serviceBase";
-
+import { IWeatherResponse } from "../models/IWeatherResponse";
 
 export const getQuote = async (): Promise<IQuote[]> => {
   const url = "https://type.fit/api/quotes";
@@ -23,21 +20,16 @@ export const getQuote = async (): Promise<IQuote[]> => {
   return data;
   
 };
-};
-
 
 export const generateDogs = async () => {
   const url = "https://api.thedogapi.com/v1/images/search?limit=3";
   const data = await get<IDog[]>(url);
   return data;
 };
-import { IWeatherResponse } from "../models/IWeatherResponse";
-import { get } from "./serviceBase";
+
 
 export const weatherResponse = async (): Promise<IWeatherResponse> =>{
     const url = "https://api.openweathermap.org/data/2.5/weather?lat=59.3293&lon=18.0686&appid=22c22c81677a5afd72f6f4410a4a4c57&units=metric"
     const data = await get<IWeatherResponse>(url);
     return data;
 }
-
-//api key: 22c22c81677a5afd72f6f4410a4a4c57
